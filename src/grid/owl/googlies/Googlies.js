5b67c1c46892f6c2876da5a1ac5a7a0aee1a6b50
BlakeMcD <blakemcd@gmail.com>
2024-11-28 14:35:31 +1100

    googly eyes change direction based on input

--- src/grid/owl/googlies/Googlies.js
+++ src/grid/owl/googlies/Googlies.js
@@ -3,16 +3,27 @@ import clsx from 'clsx';
 import styles from './Googlies.module.css';
 
 function Googlies({ position }) {
-    const { facing } = position;
+    const { direction, facing } = position;
+
+    const renderEye = () => {
+        return (
+            <div className={styles.eye}>
+                <div className={
+                    clsx(
+                        styles.pupil,
+                        direction === 'right' && styles.pupilsRight,
+                        direction === 'left' && styles.pupilsLeft,
+                        direction === 'up' && styles.pupilsUp,
+                        direction === 'down' && styles.pupilsDown,
+                    )}></div>
+            </div>
+        )
+    }
 
     return (
-        <div className={clsx(styles.root, facing === 'right' && styles.right)}>
-            <div className={styles.eye}>
-                <div className={styles.pupil}></div>
-            </div>
-            <div className={styles.eye}>
-                <div className={styles.pupil}></div>
-            </div>
+        <div className={clsx(styles.root, facing === 'right' && styles.eyesRight)}>
+            {renderEye()}
+            {renderEye()}
         </div>
     );
 }
